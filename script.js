7a65e655ae96b225d4def450163d408f21186f22
Rachel Foster <rlfoster@bellsouth.net>
2020-07-09 03:24:27 +0000

    Added confirmation text after item selection

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function chooseItem(chosenItem) {
-  alert("You have selected the " + chosenItem + ".");
+function chooseItem(selectedItem) {
+  var chosenItem;
+  var choiceText;
+
+  x = confirm("You have selected the " + selectedItem + ", please confirm.");
+  
+  if (x == true) {
+    chosenItem = selectedItem;
+    choiceText = "You have chosen to take the " + selectedItem + " with you on your hike.";
+  }
+  else {
+    choiceText = "You canceled your item choice. Please make another selection."
+  }
+  document.getElementById("itemChoice").innerHTML = choiceText;
 }
